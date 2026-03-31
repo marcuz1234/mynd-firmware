3e6e727b696cd9f58f12885f8db21d05bfcdb664
marcuz1234 <99892888+marcuz1234@users.noreply.github.com>
2026-03-31 17:19:23 +0200

    richtig DEF

--- Projects/Mynd/src/tasks/bluetooth/task_bluetooth.cpp
+++ Projects/Mynd/src/tasks/bluetooth/task_bluetooth.cpp
@@ -1,3 +1,9 @@
+// Am Anfang der Datei:
+static uint32_t bt_disconnect_timer_ms = 0;
+static bool bt_powered = true;
+#define BT_AUTO_OFF_TIMEOUT_MS 120000  // 2 Minuten = 120000 ms
+#define BT_TASK_LOOP_INTERVAL_MS 1000  // Annahme: 1s Intervall
+
 // Due to the flash size limit, only the WARNING level is available
 // for use in the complete firmware (including the bootloader).
 #if defined(BOOTLOADER)
