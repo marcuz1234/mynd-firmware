1184fa7adde29c4ac029a2c0cdaff0db68496844
marcuz1234 <99892888+marcuz1234@users.noreply.github.com>
2026-03-31 16:05:28 +0200

    BT power down

--- Projects/Mynd/src/tasks/audio/task_audio.cpp
+++ Projects/Mynd/src/tasks/audio/task_audio.cpp
@@ -780,7 +780,15 @@ static const GenericThread::Config<AudioMessage> threadConfig = {
 
                             // Both amps' datasheets specify that the PDN pin should be high for at least 5 ms
                             // before the I2S clocks start (provided by the BT module, synchronized by the system task)
-                            board_link_amps_enable(true);
+                            
+                            //board_link_amps_enable(true);
+                            if (active_audio_source == AUX_INPUT || active_audio_source == USB_INPUT) {
+                                board_link_amps_enable(true);
+                            } else if (active_audio_source == BLUETOOTH_INPUT && bt_powered) {
+                                board_link_amps_enable(true);
+                            } else {
+                                board_link_amps_enable(false);
+                            }
 
                             // Likely not necessary considering all the stuff that needs to happen
                             // before the amps are initialized when we get PowerState::On
